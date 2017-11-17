f589d1024a97998e0b397c5c6abecff845bb24be
Pavel Chukhlebov <indisguiseweare@gmail.com>
2017-11-17 11:19:35 +0500

    up
    up

---
+++ Lab04/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Таблица многочленов */
+            
+            Console.WriteLine("Введите степень, до которой будет построена таблица неприводимых многочленов:");
+            int exp = int.Parse(Console.ReadLine());
+
+            List<ZTwo> p = new List<ZTwo>()
+            {
+                ZTwo.ParseFromBits("10"),
+                ZTwo.ParseFromBits("11")
+            };
+
+            UInt64 maxValue = Convert.ToUInt64(String.Empty.PadLeft(exp + 1, '1'), 2);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            for (UInt64 i = 5; i <= maxValue; i++)
+            {
+                if (i % 2 == 0) { continue; };
+
+                bool simpleNum = true;
+
+                foreach (ZTwo z in p)
+                {
+                    if (ZTwo.mod(i, z.Value) == 0)
+                    {
+                        simpleNum = false;
+                        break;
+                    }
+                }
+
+                if (simpleNum)
+                {
+                    p.Add(new ZTwo() { Value = i });
+                }
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("Таблица неприводимых многочленов до степени {0} включительно:", exp);
+
+            foreach (ZTwo z in p)
+            {
+                Console.WriteLine("{0} : {1}", z.ToString(), z.Value);
+            }
+
+            /* Обратный элемент */
+            /*
+            Console.WriteLine("\n\nВведите многочлен X:");
+            ZTwo x = new ZTwo() { Value = Convert.ToUInt64(Console.ReadLine(), 2) };
+
+            Console.WriteLine("Введите многочлен MOD:");
+            ZTwo mod = new ZTwo() { Value = Convert.ToUInt64(Console.ReadLine(), 16) };
+
+            if (p.Contains(mod))
+            {
+                Console.WriteLine("Обратный многочлен не найти, если MOD не является неприводимым.");
+                Console.ReadLine();
+                return;
+            }
+
+            ZTwo y = new ZTwo() { Value = 1 };
+
+            while (ZTwo.mod((x * y).Value, mod.Value) != 1)
+            {
+                Console.WriteLine("{0} * {1} mod {2} = {3}", x, y, mod,
+                    new ZTwo() { Value = ZTwo.mod((x * y).Value, mod.Value) });
+
+                y.Value += 1;
+            }
+
+            Console.WriteLine("Обратный элемент для {0} по модулю {1}:\n{2}", x, mod, y);
+
+            Console.WriteLine("{0} * {1} mod {2} = {3}", x, y, mod,
+                new ZTwo() { Value = ZTwo.mod((x * y).Value, mod.Value) });
+
+            */
+            Console.WriteLine("Время выполнения {0}", sw.Elapsed);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab04/ZTwo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05
+{
+    class ZTwo
+    {
+        public UInt64 Value;
+
+        private static int BitsInNum(UInt64 num)
+        {
+            return Convert.ToString((Int64)num, 2).Length;
+        }
+
+        public static UInt64 mod(UInt64 val, UInt64 del)
+        {
+            if (val < del) return val;
+
+            while (val >= del)
+            {
+                UInt64 mask = del << (BitsInNum(val) - BitsInNum(del));
+                val ^= mask;
+            }
+
+            return val;
+        }
+
+        public static ZTwo operator +(ZTwo a, ZTwo b)
+        {
+            return new ZTwo() { Value = (a.Value ^ b.Value) };
+        }
+
+        public static ZTwo operator *(ZTwo a, ZTwo b)
+        {
+            UInt64 result = 0, uint_a = a.Value, uint_b = b.Value;
+
+            //"обычное" умножение
+            for (int i = 0; i < 8; i++)
+            {
+                result ^= (UInt64)(uint_a * (uint_b & ((UInt64)1 << i)));
+            }
+
+            //return new ZTwo() { Value = mod(result, 283) };
+            return new ZTwo() { Value = result };
+        }
+
+        public override string ToString()
+        {
+            return Convert.ToString((Int64)this.Value, 2);
+        }
+
+        public static ZTwo ParseFromBits(string bits)
+        {
+            return new ZTwo() { Value = Convert.ToUInt64(bits, 2) };
+        }
+    }
+}
